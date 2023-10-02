29050328a3f4169972221d43f721da2edcff727d
amine <amine.fadleddine753@gmail.com>
2023-10-02 15:09:31 +0100

    python.developpez.com - Exo 1

---
+++ python.developpez.com/exo-1.py
@@ -0,0 +1,8 @@
+phrase = input("Please enter a phrase and we'll try search for occurrence of letter 'e' : \n")
+
+print("Searching for e occurence in the phrase !")
+
+if 'e' in phrase:
+    print("Found 'e' occurence in the phrase !")
+else:
+    print("The phrase does not have any occurrence of letter 'e' !")
